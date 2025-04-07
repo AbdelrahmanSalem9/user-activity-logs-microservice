fe7f0bc8e9ad73fc46400bacf36bc10487cf7ca9
AbdelrahmanSalem9 <Abdelrahmansalem16@gmail.com>
2025-04-07 21:35:02 +0200

    prevent producer to create new topic

--- consumer-producer/producer.js
+++ consumer-producer/producer.js
@@ -2,15 +2,15 @@ require("dotenv").config();
 const { Kafka } = require("kafkajs");
 const { generateRandomLog } = require("../custom-utils/log-utils");
 
-
 const writeRate = process.env.PRODUCER_LOG_RATE;
 const kafkaHost = process.env.KAFKA_HOST || "kafka";
+const topic = process.env.TOPIC || "transaction-producer";
 
 const kafka = new Kafka({
   clientId: "transaction-producer",
   brokers: [`${kafkaHost}:9092`],
 });
-const producer = kafka.producer();
+const producer = kafka.producer({ allowAutoTopicCreation: false });
 
 async function produceLog() {
   try {
@@ -19,7 +19,7 @@ async function produceLog() {
     while (true) {
       const log = generateRandomLog();
       await producer.send({
-        topic: "transaction-activity",
+        topic: topic,
         messages: [{ value: JSON.stringify(log) }],
       });
 
